310f0bfb1d69bc2144b6a8edd4fd813ccc2fb87d
David Pérez <jdperezdu@ufpso.edu.co>
2025-11-30 12:29:30 -0500

    se ha agregado el logo y se ha creado el login de la página

--- the-light-gallery/src/App.jsx
+++ the-light-gallery/src/App.jsx
@@ -1,12 +1,61 @@
+// src/App.jsx
 import './App.css'
 
 function App() {
-  // Las clases de Bootstrap "bg-dark" (fondo oscuro) y "text-white" (letra blanca)
-  // min-vh-100 asegura que ocupe toda la altura de la pantalla.
+  // Las clases de Bootstrap ('d-flex', 'align-items-center', 'justify-content-center')
+  // se usan para centrar el formulario de Login en la pantalla.
   return (
-    <div className="container-fluid bg-dark text-white min-vh-100">
-      <h1 className="text-center p-3">The Light Gallery</h1>
-      {/* Todo el contenido funcional de la aplicación irá aquí */}
+    <div className="container-fluid bg-black text-white min-vh-100 d-flex align-items-center justify-content-center"
+      style={{
+    backgroundImage: 'url(/fondo.png)', // Asegúrate de que el nombre de la imagen sea correcto
+    backgroundSize: 'cover', // Cubrirá todo el espacio
+    backgroundPosition: 'center', // Centra la imagen
+  }}
+    >
+      {/* Tarjeta de Login (Card) */}
+      <div className="card shadow p-4 text-center"
+        style={{ 
+        width: '20rem', 
+        // CAMBIO AQUÍ: (R=255, G=255, B=255, Alpha=0.9 para 90% opacidad)
+        backgroundColor: 'rgba(255, 255, 255, 0.3)', 
+        // Asegura que los bordes de los inputs se vean
+        border: '1px solid rgba(0, 0, 0, 0.2)' 
+    }}
+      >
+       <h1 
+        className="card-title text-black mb-4 fw-bold" 
+        style={{ fontFamily: "'Cinzel', serif", fontSize: '2.5rem' }}
+        >Cine Leonelda Ocaña
+       </h1>
+        <p className='text-start text-black'>El cine como nunca lo has vivido en Ocaña.</p>
+        <p></p>
+        <form>
+          <div className="mb-3">
+            <input 
+              type="text" 
+              className="form-control" 
+              placeholder="Usuario" 
+            />
+          </div>
+          <div className="mb-3">
+            <input 
+              type="password" 
+              className="form-control" 
+              placeholder="Contraseña" 
+            />
+          </div>
+          <button 
+            type="submit" 
+            className="btn btn-danger mt-3 w-100">
+            INGRESAR
+          </button>
+        </form>
+{/* Enlace de Registro: Enlace de "Crear Cuenta" usando el color rojo */}
+<p className="mt-3 mb-0 text-black">
+  ¿No tienes cuenta? <a href="#" className="text-white fw-bold">Crear Cuenta</a>
+</p>
+        
+      </div>
     </div>
   )
 }
